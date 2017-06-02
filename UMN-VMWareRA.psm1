2308572d23a571857e04e8b224d58c1a67a66037
tjsobeck <tjsobeck@umn.edu>
2017-06-02 13:44:30 -0500

    Add connect/disconnect functions
    Add connect/disconnect functions

--- UMN-VMWareRA.psm1
+++ UMN-VMWareRA.psm1
@@ -13,3 +13,92 @@
 
 # You should have received a copy of the GNU General Public License
 # along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+#region Connect-VMWareRASession
+function Connect-VMWareRASession {
+<#
+    .Synopsis
+        Get IPv4 from vmware if VM is running with integration tools installed
+    .DESCRIPTION
+        Long description
+    .PARAMETER vCenter
+
+    .PARAMETER vmwareCreds
+    .EXAMPLE
+        a
+    .EXAMPLE
+    A   nother example of how to use this cmdlet
+#>
+    [CmdletBinding()]
+    Param
+    (
+        [ValidateNotNullOrEmpty()]
+        [string]$vCenter,
+
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]$vmwareCreds
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        ## Convert the Pscred into a basic auth hash
+        $auth = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($vmwareCreds.UserName+':'+$vmwareCreds.GetNetworkCredential().Password))
+        ## Construct url
+        $url = "https://$vCenter/rest/com/vmware/cis/session"
+        $return = Invoke-WebRequest -Uri $url -Method Post `
+            -Headers @{'vmware-use-header-authn' = 'asdfypaf';'vmware-api-session-id'='null';'Accept' = 'application/json';'Authorization' = "Basic $auth"} `
+            -ContentType 'application/json'
+        if($return.StatusCode -ne 200){throw "Failed to login $return"}
+        return ($return.Content | convertfrom-json).value
+    }
+    End
+    {
+    }
+}
+#endregion
+
+#region Disconnect-VMWareRASession 
+function Disconnect-VMWareRASession {
+<#
+    .Synopsis
+        Get IPv4 from vmware if VM is running with integration tools installed
+    .DESCRIPTION
+        Long description
+    .PARAMETER vCenter
+
+    .PARAMETER vmwareCreds
+    .EXAMPLE
+        a
+    .EXAMPLE
+    A   nother example of how to use this cmdlet
+#>
+    [CmdletBinding()]
+    Param
+    (
+        [ValidateNotNullOrEmpty()]
+        [string]$vCenter,
+        
+        [ValidateNotNullOrEmpty()]
+        [string]$sessionID
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        ## Construct url
+        $url = "https://$vCenter/rest/com/vmware/cis/session"
+        $return = Invoke-WebRequest -Uri $url -Method Delete  `
+            -Headers @{'vmware-api-session-id'=$sessionID;'Accept' = 'application/json'} `
+            -ContentType 'application/json'
+        if($return.StatusCode -ne 200){throw "Failed to logout"}
+    }
+    End
+    {
+    }
+}
+#endregion
